4a602e8a2e694af2423bc88d44ab03161d952260
TabbieManywanda <achiengtabitha@gmail.com>
2023-06-21 20:42:42 +0300

    add 3 new endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-// import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 // import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 
@@ -18,6 +18,18 @@ function controllerRouting(app) {
 
   router.post('/users', (req, res) => {
     UsersController.postNew(req, res);
+  });
+
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 }
 
